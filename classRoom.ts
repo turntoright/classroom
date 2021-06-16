ba618c02aa2c7766bf0e94e99df291d9976ec50a
turntoright <hongwucao1975@gmail.com>
2021-06-16 20:48:08 +1200

    Classroom class

---
+++ classRoom.ts
@@ -0,0 +1,40 @@
+// room type enum
+enum RoomType {
+    SharedWorkspace = 'Shared Workspace',
+    Workshop = 'Workshop',
+    Auditorium = 'Auditorium',
+    RestRoom = 'Rest Room',
+    Gym = 'Gym',
+    Others = 'Others'
+}
+
+// Funiture class
+class Funiture {
+    name: string;
+    amount: number;
+
+    constructor(name: string, amount: number) {
+        this.name = name;
+        this.amount = amount;
+    }
+}
+
+// Classroom class
+class ClassRoom {
+    roomType: RoomType;
+    furnitures: Funiture[];
+    buildings: string[];
+
+    constructor(roomType: RoomType, funitrues: Funiture[], buildings: string[]) {
+        this.roomType = roomType;
+        this.furnitures = funitrues;
+        this.buildings = buildings;
+    }
+}
+
+/* 
+let funiture1 = new Funiture('chairs', 10);
+let funiture2 = new Funiture('desk', 8);
+let room1 = new ClassRoom(RoomType.Workshop, [funiture1, funiture2], ['building1', 'building2']);
+console.log(room1);
+*/
